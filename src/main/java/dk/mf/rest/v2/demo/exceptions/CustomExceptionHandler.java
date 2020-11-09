c65e34374cc2fa93fa4572f93dbef3bfc304f428
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-11-09 09:21:16 +0100

    Gitignore

--- src/main/java/dk/mf/rest/v2/demo/exceptions/CustomExceptionHandler.java
+++ src/main/java/dk/mf/rest/v2/demo/exceptions/CustomExceptionHandler.java
@@ -13,22 +13,22 @@ import java.util.Date;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        CustomError errorDetails = new CustomError(new Date(), "Ups! Your resource is missing, sorry client!", request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-    }
+//    @ExceptionHandler(ResourceNotFoundException.class)
+//    public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+//        CustomError errorDetails = new CustomError(new Date(), "Ups! Your resource is missing, sorry client!", request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+//    }
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<?> customExceptionHandler(Exception ex, WebRequest request) {
-        CustomError errorDetails = new CustomError(new Date(), "Ups! Something is wrong here, sorry client!", request.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<?> customExceptionHandler(Exception ex, WebRequest request) {
+//        CustomError errorDetails = new CustomError(new Date(), "Ups! Something is wrong here, sorry client!", request.getDescription(false));
+//        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        CustomError errorDetails = new CustomError(new Date(), "Validation Failed", ex.getBindingResult().toString());
-        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
-    }
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//        CustomError errorDetails = new CustomError(new Date(), "Validation Failed", ex.getBindingResult().toString());
+//        return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
+//    }
 
 }
